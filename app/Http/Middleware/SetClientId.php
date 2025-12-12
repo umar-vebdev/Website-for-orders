cb7216c21105ca0f3d807fbe619608652671ee52
Umar <umarbobojonzoda@gmail.com>
2025-12-12 18:01:02 +0500

    change admin registration

--- app/Http/Middleware/SetClientId.php
+++ app/Http/Middleware/SetClientId.php
@@ -5,15 +5,19 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Http\Request;
 
 class SetClientId
 {
-    public function handle($request, Closure $next)
-    {
-        if (! $request->cookie('client_id')) {
-            Cookie::queue('client_id', Str::uuid(), 60 * 24 * 365);
-        }
-
-        return $next($request);
+    public function handle(Request $request, Closure $next)
+{
+    if (! $request->cookie('client_id')) {
+        Cookie::queue('client_id', Str::uuid(), 60 * 24 * 365); // 1 год
     }
+
+    $response = $next($request);
+
+    return $response; 
+}
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,28 +21,6 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
