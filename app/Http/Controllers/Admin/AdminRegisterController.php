ac4f0570491e872cebcf465dd29f84535ae509b0
Umar <umarbobojonzoda@gmail.com>
2025-12-12 16:49:17 +0500

    add AdminRegisterController

--- app/Http/Controllers/Admin/AdminRegisterController.php
+++ app/Http/Controllers/Admin/AdminRegisterController.php
@@ -4,8 +4,30 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminRegisterController extends Controller
 {
-    //
+    public function showForm() 
+    {
+        return view('admin.register');
+    }
+
+    public function register(Request $request) 
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'is_admin' => 1,
+        ]);
+        return redirect(); //Еще не готово
+    }
 }
